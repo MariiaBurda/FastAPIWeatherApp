6f076eb5387f3ba7dbf7974fbcfaf53dbca94502
Mariia Burda <mariiaburda93@gmail.com>
2024-12-20 15:31:32 +0200

    Add cash checking for requested city

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from dotenv import load_dotenv
 import os
 import json
 import aioboto3
-from datetime import datetime
+from datetime import datetime, timezone, timedelta
 
 load_dotenv()
 
@@ -22,13 +22,43 @@ DYNAMODB_TABLE = os.getenv("DYNAMODB_TABLE")
 
 BASE_URL = "https://api.openweathermap.org/data/2.5/weather"
 
+async def check_cache(city: str):
+    session = aioboto3.Session()
+    async with session.client(
+        "s3",
+        aws_access_key_id=AWS_ACCESS_KEY_ID,
+        aws_secret_access_key=AWS_SECRET_ACCESS_KEY,
+        region_name=AWS_REGION,
+    ) as s3:
+        try:
+            # get a list of objects that match the city_* pattern
+            response = await s3.list_objects_v2(Bucket=S3_BUCKET_NAME, Prefix=f"{city}_")
+            if "Contents" not in response:
+                return None
+
+            # find the newest file
+            files = response["Contents"]
+            latest_file = max(files, key=lambda x: x["LastModified"])
+
+            # get data if last modification time is less than 5 minutes
+            last_modified = latest_file["LastModified"].replace(tzinfo=timezone.utc)
+            if datetime.now(timezone.utc) - last_modified < timedelta(minutes=5):
+                obj = await s3.get_object(Bucket=S3_BUCKET_NAME, Key=latest_file["Key"])
+                data = await obj["Body"].read()
+                return json.loads(data)
+            return None
+        except Exception as e:
+            print(f"Error checking cache: {e}")
+            return None
+
+
 async def log_weather_event(city: str, timestamp: str, s3_url: str):
     session = aioboto3.Session()
     async with session.client(
-            "dynamodb",
-            region_name=AWS_REGION,
-            aws_access_key_id=AWS_ACCESS_KEY_ID,
-            aws_secret_access_key=AWS_SECRET_ACCESS_KEY,
+        "dynamodb",
+        region_name=AWS_REGION,
+        aws_access_key_id=AWS_ACCESS_KEY_ID,
+        aws_secret_access_key=AWS_SECRET_ACCESS_KEY,
     ) as dynamodb:
         await dynamodb.put_item(
             TableName=DYNAMODB_TABLE,
@@ -57,6 +87,15 @@ async def save_to_s3(filename: str, data: dict):
 @app.get("/weather")
 async def get_weather(city: str = Query(..., min_length=1)):
     try:
+        cached_data = await check_cache(city)
+        if cached_data:
+            return {
+                "source": "cache",
+                "city": cached_data["name"],
+                "temperature": cached_data["main"]["temp"],
+                "description": cached_data["weather"][0]["description"],
+            }
+
         async with httpx.AsyncClient() as client:
             response = await client.get(
                 BASE_URL,
